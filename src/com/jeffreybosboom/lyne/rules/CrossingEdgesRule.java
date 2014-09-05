022c0f4a405ddf3f6d15b2df081c8cc1f59e252a
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-09-05 13:43:58 -0400

    Remove stale TODO in CrossingEdgesRule

--- src/com/jeffreybosboom/lyne/rules/CrossingEdgesRule.java
+++ src/com/jeffreybosboom/lyne/rules/CrossingEdgesRule.java
@@ -13,7 +13,6 @@ import java.util.Iterator;
 public final class CrossingEdgesRule implements InferenceRule {
 	@Override
 	public Puzzle apply(Puzzle puzzle) {
-		//TODO: actually need to check crossing edge is assigned etc
 		for (Iterator<Pair<Node, Node>> i = puzzle.edges().iterator(); i.hasNext();) {
 			Pair<Node, Node> p = i.next();
 			if (p.first.row() == p.second.row() || p.first.col() == p.second.col())
