014fcc67805e7d82230a25c3a3a248fad96ac841
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-09-06 10:52:22 -0400

    ContradictionException: no stack traces, comments

--- src/com/jeffreybosboom/lyne/ContradictionException.java
+++ src/com/jeffreybosboom/lyne/ContradictionException.java
@@ -1,13 +1,16 @@
 package com.jeffreybosboom.lyne;
 
 /**
- *
+ * Thrown when a Puzzle operation would result in an unsolvable puzzle.
  * @author Jeffrey Bosboom <jbosboom@csail.mit.edu>
  * @since 8/16/2014
  */
-public class ContradictionException extends RuntimeException {
-
-	public ContradictionException() {
+public final class ContradictionException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	public ContradictionException() {}
+	@Override
+	public synchronized Throwable fillInStackTrace() {
+		//for performance, don't bother with stack traces
+		return this;
 	}
-
 }
