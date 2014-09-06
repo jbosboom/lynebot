0e4d77eb984fb4154be95c5dc29d7a4451c5debd
Jeffrey Bosboom <jbosboom@csail.mit.edu>
2014-09-06 11:13:05 -0400

    License and license headers

--- src/com/jeffreybosboom/lyne/Node.java
+++ src/com/jeffreybosboom/lyne/Node.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2014 Jeffrey Bosboom.
+ * This file is part of lynebot.
+ *
+ * lynebot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lynebot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lynebot.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.jeffreybosboom.lyne;
 
 import com.google.common.base.Preconditions;
